640a904cbba9ff5abf28b8c7f23aa007a9359247
arunPdl02 <aroon.paudel@gmail.com>
2026-03-01 10:16:41 -0800

    fixed typescirpt errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -114,7 +114,7 @@ const pageTransition = {
   initial: { opacity: 0, x: 10 },
   animate: { opacity: 1, x: 0 },
   exit: { opacity: 0, x: -10 },
-  transition: { duration: 0.2, ease: "easeInOut" },
+  transition: { duration: 0.2, ease: "easeInOut" as const },
 };
 
 function RoutesConfig() {
